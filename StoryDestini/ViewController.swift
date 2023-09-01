5eeec1c1f27751ac546292a82fec3a02fb932375
jeff-0324 <jeffap324@gmail.com>
2023-09-01 15:47:57 +0900

    Initial Commit

---
+++ StoryDestini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryDestini
+//
+//  Created by jae hoon lee on 2023/09/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
